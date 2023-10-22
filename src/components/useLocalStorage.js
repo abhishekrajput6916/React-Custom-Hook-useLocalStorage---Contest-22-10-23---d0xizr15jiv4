0455f2d05e6913d03ce8395c7a97248210746ceb
Abhishek Rajput <98444175+abhishekrajput6916@users.noreply.github.com>
2023-10-22 11:19:29 +0530

    Update useLocalStorage.js

--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -2,7 +2,26 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const useLocalStorage = (stateVar, value) => {
-  
+  const [lsValue, setLsValue] = useState(() => {
+    if (value) {
+      return JSON.parse(value);
+    } else {
+      localStorage.setItem(stateVar, JSON.stringify(value));
+      return value;
+    }
+  });
+  function setLSState(valorfn) {
+    let newVal;
+    if (typeof valorfn === "function") {
+      const fn = valorfn;
+      newVal = fn(lsValue);
+    } else {
+      newVal = valorfn;
+    }
+    localStorage.setItem(key, JSON.stringify(newVal));
+    setLsValue(newVal);
+  }
+  return [lsValue, setLsValue];
 };
 
 export default useLocalStorage;
